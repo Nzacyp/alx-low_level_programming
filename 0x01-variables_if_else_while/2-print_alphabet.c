ddef45cd9504562a1cee9ba38c61d0dc817835b4
nzacyp <nzacyp2@gmail.com>
2023-03-20 06:35:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstvwxwy"; 
+
+	for (i = 0; i < 26;i++)
+        { 
+		putchar(alp[i]);
+        }
+	putchar('\n');
+	return (0);
+}
