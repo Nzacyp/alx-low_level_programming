c5d6b05b5865604f7f21a2bf3fd376b397334131
nzacyp <nzacyp2@gmail.com>
2023-03-23 03:26:43 -0700

    advenced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - program compilation begins from main
+* Return: 0 always(Success)
+*/
+int main(void)
+{
+int a, b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 49; b <= 57; b++)
+{
+if ( a != b && b > a)
+{
+putchar(a);
+putchar(b);
+putchar(',');
+}
+}
+}
+putchar('\n');
+return (0);
+}
