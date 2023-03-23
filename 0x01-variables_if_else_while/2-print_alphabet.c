84e5540c48a598bafd4a4ddef3edc14091ee0cf5
nzacyp <nzacyp2@gmail.com>
2023-03-23 00:26:52 -0700

    yes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
+* main - main function
+*
+* Return: Always 0
+*/
 int main(void)
 {
-       char alp[26] = "abcdefghijklmnopqrstuvwxwy";
-       int i;	
-
-       for (i = 0; i < 26;i++)
-       { 
-	      putchar(alp[i]);
-       }
-	putchar('\n');
-	return (0);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
 }
