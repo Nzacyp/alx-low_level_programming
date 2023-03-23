aad431152bb71f98ac31c0a908bdc8bdf7f3381a
nzacyp <nzacyp2@gmail.com>
2023-03-23 00:53:42 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: sammy
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int i;
+for (i = 0; a < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
