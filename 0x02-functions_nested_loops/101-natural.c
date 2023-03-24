c47c5e458703cc8abb98ef6d829af45c270869b5
nzacyp <nzacyp2@gmail.com>
2023-03-24 03:35:56 -0700

    naturel

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
+
 /**
  * main - Entry point
  *
@@ -8,6 +7,16 @@
  */
 int main(void)
 {
-
-	return (0);
+int i;
+int b;
+b = 0;
+for (i = 0 ; i < 1024 ; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+b = b + i;
+}
+}
+printf("%d\n", b);
+return (0);
 }
