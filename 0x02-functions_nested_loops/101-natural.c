ef1b92a4ba9b2222b655341d17c3a596f570ca2b
nzacyp <nzacyp2@gmail.com>
2023-03-24 03:19:00 -0700

    naturel

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int i;
-int sum;
-sum = 0;
+int b;
+b = 0;
 for (i = 0 ; i < 10 ; i++)
 {
 if ((i % 3 == 0) || (i % 5 == 0))
 {
-sum = sum + i;
+b = b + i;
 }
 }
-printf("%d\n", sum);
+printf("%d\n", b);
 return (0);
 }
