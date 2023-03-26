742e6c05954629af139a98922ccb93037471ee8f
nzacyp <nzacyp2@gmail.com>
2023-03-26 01:34:28 -0700

    most number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-* print_numbers - prints  numbers from 0 to 9
-*
-* Return: Always 0
-*/
+ * print_numbers - from 0 to 9, followed by a new line.
+ * Return: Always 0
+ */
+
 void print_numbers(void)
 {
-char c;
+	int n;
 
-for (c = '0'; c <= '9'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (n = 48; n < 58; n++)
+	{
+	      printf(n);
+	}
+	_putchar('\n');
 }
