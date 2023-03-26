e3ec56ec97305a362466819d96f539aa476ea30f
nzacyp <nzacyp2@gmail.com>
2023-03-26 03:47:58 -0700

    print combs

---
+++ 100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b<= '9'; b++)
+		{
+			if (a < b)
+			{
+			putchar(a);
+			putchar(b);
+			if (a == '8' && b == '9')
+			{
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+		putchar('\n');
+	return (0);
+}
