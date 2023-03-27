beae9e1100f8e5235541ebe029dc7b63c45d5209
nzacyp <nzacyp2@gmail.com>
2023-03-27 04:24:55 -0700

    reset

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
- * @n: pointer to the variable to update
- */
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and update the value it points to to 98
+ * @n: input
+ *return: n
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
