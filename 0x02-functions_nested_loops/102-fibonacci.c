6368071b93f107ae6edcfa3d99d9f21759559c7f
nzacyp <nzacyp2@gmail.com>
2023-03-28 09:48:49 -0700

    fibrocii

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,36 +1,35 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - main block
- * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
- * Numbers must be coma and space separated.
+ * main - prints first 50 numbers of the fibonacci sequence
+ *
+ * Description: prints from 1 to 50
  * Return: 0
  */
 
 int main(void)
 {
-int c = 3;
-long int a  = 1, b = 2;
-long int n = a + b;
+	int i, n, first_count, sec_count, next_num;
 
+	first_count = 1;
+	sec_count = 2;
+	n = 50;
 
-while (c <= 50)
-{
-printf("%d, ", a);
-printf("%d, ", b);
-}
-if (c == 50)
-{
-printf("%d \n", n);
-}
-else
-{
-printf("%d, ", n);
-}
-a = b;
-b = n;
-n = a + b;
-c++;
-
-return (0);
+	for (i = 0; i < n; i++)
+	{
+		if (i == 1)
+		{
+			printf("%d", first_count);
+		}
+		else
+		{
+			printf("%d, ", first_count);
+			next_num = first_count + sec_count;
+			first_count = sec_count;
+			sec_count = next_num;
+		}
+	}
+	printf("\n");
+	return (0);
 }
