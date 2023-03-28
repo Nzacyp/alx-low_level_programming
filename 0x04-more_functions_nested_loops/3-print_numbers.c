945c7f350c5847de9fe3ab32bcac443a57fe53c1
nzacyp <nzacyp2@gmail.com>
2023-03-28 08:58:03 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_numbers - from 0 to 9, followed by a new line.
- * Return: Always 0
+ * print_numbers - print numbers
+ *
+ * Description: prints the numbers, from 0 to 9, followed by a new line
+ * Return: Always (0)
  */
 
 void print_numbers(void)
 {
-	int n;
+	char i;
 
-	for (n = 48; n < 58; n++)
+	for (i = 0; i <= 9; i++)
 	{
-	      printf(n);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
