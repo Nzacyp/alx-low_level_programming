7cb87d3f60845949cd17ebf6bd33e29ac869abde
nzacyp <nzacyp2@gmail.com>
2023-04-03 06:43:27 -0700

    all

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,25 +2,22 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+  * print_diagsums - Print the sum of two diagonals of square matrix
+  *
+  * @a: the matrix
+  * @size: the size
+  *
+  * Return: Nothing.
+  */
+void print_diagsums(int *a, int size)
 {
-int c3[3][3] = {
-{0, 1, 5},
-{10, 11, 12},
-{1000, 101, 102},
-};
-int c5[5][5] = {
-{0, 1, 5, 12124, 1234},
-{10, 11, 12, 123521, 12512},
-{1000, 101, 102, 12545, 214543435},
-{100, 1012451, 11102, 12545, 214543435},
-{10, 12401, 10452, 11542545, 1214543435},
-};
-print_diagsums((int *)c3, 3);
-print_diagsums((int *)c5, 5);
-return (0);
+	int b, sum1 = 0, sum2 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
